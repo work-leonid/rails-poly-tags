4c3e75725f8b4617261567c6a19e7bf99dac0036
work-leonid <work.leonid@gmail.com>
2021-12-28 09:36:56 +0300

    delete comments and add link to tag from book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,4 @@ class Book < ApplicationRecord
   has_many :taggings, as: :taggable, dependent: :destroy
   has_many :tags, through: :taggings
 
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :books
   resources :courses
   resources :tags
 
-  # Defines the root path route ("/")
   root "books#index"
 end
